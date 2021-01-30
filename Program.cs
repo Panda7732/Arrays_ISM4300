206344051cf6d7bfd4ec7ca6c95ded10f6a219fe
Amanda Hernandez <amandah2@usf.edu>
2021-01-30 16:26:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Name: Amanda Hernandez
+ * Date: 1/20/2021
+ * Description: Arrays
+ */
+using System;
+
+namespace Arrays_ISM4300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //use for loop to iterate the line "element value =i"  where i goes from 1 to 25
+           for(int i = 0; i< 26; i++)
+            {
+                
+                    Console.WriteLine("Element Value =" + i);
+
+            }
+
+
+        }
+    }
+}
